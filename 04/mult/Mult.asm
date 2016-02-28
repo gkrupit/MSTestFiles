c4ef52aacb4c33421019c552552c7e47f2b0dd85
Eleni Mijalis <elenimijalis@gmail.com>
2016-02-28 07:27:31 -0600

    multiplier complete

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@1
+D=M
+@i // iterator
+M=D // RAM[i] = RAM[1]
+
+@prod
+M=0 // make sure product initialized to 0
+
+(LOOP) // continue adding RAM[0] to prod until i == 0
+    // if i == 0 goto END
+    @i
+    D=M
+    @STOP
+    D;JEQ
+
+    @0
+    D=M
+    @prod
+    M=D+M
+
+    // i = i - 1
+    @i
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(STOP)
+    @prod
+    D=M
+    @2
+    M=D
+
+(END)
+    @END
+    0;JMP
